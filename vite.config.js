9f06ec8e3fa030ff8ac7d0aeca878b66e91833be
anilkumar9493 <vadlaanil225@gmail.com>
2026-04-16 16:42:29 +0530

    remove incompatible pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,12 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'ToDo App',
-        short_name: 'ToDo',
-        description: 'Simple task manager app',
-        theme_color: '#2563eb',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/ToDo-App/',
-        scope: '/ToDo-App/',
-        icons: [
-          {
-            src: '/ToDo-App/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/ToDo-App/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
   ],
   server: {
     open: true,
